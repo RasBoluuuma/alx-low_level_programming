e6fcc0795120ed56fe0a7357bce047c5f26842ce
root <root@localhost.localdomain>
2022-09-20 00:39:25 +0100

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - Prints a string followed by a new line
+ * @str: String to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		++str;
+	}
+	_putchar('\n');
+}
